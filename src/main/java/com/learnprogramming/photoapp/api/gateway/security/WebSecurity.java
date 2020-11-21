edff7febe76af09c030b575a52b5ce872f8f6a38
manish978973 <manish.rama@ovgu.de>
2020-11-21 19:22:26 +0100

    Minor changes to authorization paths via zuul

--- src/main/java/com/learnprogramming/photoapp/api/gateway/security/WebSecurity.java
+++ src/main/java/com/learnprogramming/photoapp/api/gateway/security/WebSecurity.java
@@ -18,38 +18,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	}
 	
 	protected void configure (HttpSecurity http) throws Exception {
-		/*
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
-		http.authorizeRequests()
-		.antMatchers(environment.getProperty("api.users.actuator.url.path")).permitAll()
-		.antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
-		.antMatchers(HttpMethod.POST, "/user-microservice/").permitAll()
-        .antMatchers(HttpMethod.POST,"/user-microservice/authenticate").permitAll()
-		.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
-		.antMatchers(HttpMethod.POST,environment.getProperty("api.registration.url.path")).permitAll()
-		.antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.addFilter(new AuthorizationFilter(authenticationManager(),environment));
-		
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);  */
-		
+
 		http.cors().disable();
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
-              //  .antMatchers(environment.getProperty("api.h2.url.path")).permitAll()
                .antMatchers(HttpMethod.POST,environment.getProperty("api.registration.url.path")).permitAll()
                .antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
+				/*
                 .antMatchers(HttpMethod.GET,"/vehicle/fetch").permitAll()
                 .antMatchers(HttpMethod.GET,"/position/fetch/village_truck").permitAll()
                 .antMatchers(HttpMethod.GET,"/test-microservice/rest/publish/fetchid").permitAll()
-          //      .antMatchers("/**").permitAll()
-              //  .antMatchers("/user-microservice/").permitAll()
-       // .antMatchers(HttpMethod.POST,"/**").permitAll()
-      
-                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()  */
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new AuthorizationFilter(authenticationManager(),environment));
